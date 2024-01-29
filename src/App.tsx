7585966deb7abda4f8d9798ca574d1ab8f3bf496
Yusmin97 <tmdamsi97@gmail.com>
2024-01-29 11:28:24 +0900

    ⚗️ Test: 로그인 페이지 amplify 연습용으로 배포 하기 위한 임시 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="cat_logo"></div>
+        <div className="login">
+          <button className="login_btn">로그인</button>
+        </div>
+        <div className="join">
+          <button className='join_btn'>계정 없을 시 | 회원가입</button>
+        </div>        
+      </div>
     </div>
   );
 }
