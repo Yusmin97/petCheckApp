ceb523e48bbcf4df3cc8a34281a782609013c36f
Yusmin97 <tmdamsi97@gmail.com>
2024-01-30 17:54:27 +0900

    ✨ Feat: 캘린더 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import './App.css';
+import Calendar from './page/calender/Calendar';
+
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className="cat_logo"></div>
-        <div className="login">
-          <button className="login_btn">로그인</button>
-        </div>
-        <div className="join">
-          <p className='join_p'>계정 없을 시 |</p>
-          <button className='join_btn'>회원가입</button>
-        </div>        
-      </div>
+      <Calendar />
     </div>
   );
 }
