41472e602cd50d876fb2e6e1ee4ad05ae5196777
Yusmin97 <tmdamsi97@gmail.com>
2024-02-01 10:52:22 +0900

    ✨ Feat: 로그인 컴포넌트 제작

---
+++ src/page/login/Login.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="container">
+      <div className="input_row1">
+        <input type="text" className="id" placeholder="ID"></input>
+      </div>
+      <div className="input_row2">
+        <input type="password" className="password" placeholder="Password"></input>
+      </div>
+      <div className="login">
+        <button className="login_btn">로그인</button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
