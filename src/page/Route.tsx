93440c2e0a4a070849ddabff92213c822eb1362a
Yusmin97 <tmdamsi97@gmail.com>
2024-02-01 16:58:48 +0900

    ✨ Feat: 메인 페이지 1차 구현 및 라우트 추가

--- src/page/Route.tsx
+++ src/page/Route.tsx
@@ -3,6 +3,7 @@ import FrontPage from "./front/FrontPage";
 import LoginPage from "./login/Login";
 import SignUpPage from "./signup/Signup";
 import PetInfo from "./petInfo/PetInfo";
+import Main from "./main/Main";
 
 import Calendar from "./calender/Calendar";
 
@@ -14,6 +15,7 @@ function Router() {
       <Route path="/login" element={<LoginPage />}></Route>
       <Route path="/signup" element={<SignUpPage />}></Route>
       <Route path="/petinfo" element={<PetInfo />}></Route>
+      <Route path="/main" element={<Main />}></Route>
 
       <Route path="/calendar" element={<Calendar />}></Route>
     </Routes>
---
+++ src/page/main/Main.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./Main.css"
+
+function Main() {
+  return (
+    <div className="e126_46">
+      <div className="e126_47"></div>
+      <div className="e126_55"></div>
+      <div className="e126_70"></div>
+      <div className="e128_73"></div>
+      <div className="e128_71"></div>
+      <div className="e128_72"></div>
+      <div className="e126_53"></div>
+      <div className="e126_65"></div>
+      <div className="e126_66"></div>
+      <div className="e126_68"></div>
+      <span className="e126_58">+</span>
+      <span className="e126_67">😺</span>
+      <span className="e128_75">🏥</span>
+      <span className="e128_76">🦮</span>
+      <span className="e126_69">🐶</span>
+      <span className="e131_97">캘린더</span>
+    </div>
+  );
+}
+
+export default Main;
