0b719f95bc294273813223f18cde35a6271db346
Yusmin97 <tmdamsi97@gmail.com>
2024-02-01 10:53:30 +0900

    ✨ Feat: 로그인 컴포넌트 라우트 추가

--- src/page/Route.tsx
+++ src/page/Route.tsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import FrontPage from "./front/FrontPage";
+import Login from "./login/Login";
+
 import Calendar from "./calender/Calendar";
+
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<FrontPage />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      
       <Route path="/calendar" element={<Calendar />}></Route>
     </Routes>
   )
