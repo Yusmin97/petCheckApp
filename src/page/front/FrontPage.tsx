6e0b7e3950046d5b457dd59d076f49ee049ae8ed
Yusmin97 <tmdamsi97@gmail.com>
2024-02-01 11:00:29 +0900

    🔨 Modify: FrontPage와 LoginPage 의 중복된 className 수정

--- src/page/front/FrontPage.tsx
+++ src/page/front/FrontPage.tsx
@@ -5,8 +5,8 @@ function FrontPage() {
   return (
     <div className="container">
       <div className="cat_logo"></div>
-      <div className="login">
-        <button className="login_btn">로그인</button>
+      <div className="front_login">
+        <button className="front_login_btn">로그인</button>
       </div>
       <div className="join">
         <button className="join_btn">계정 없을 시 | 회원가입</button>
