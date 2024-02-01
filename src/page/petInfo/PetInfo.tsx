b3134e4d40a1cc4ee5ec04ad82f683b61dafb3b2
Yusmin97 <tmdamsi97@gmail.com>
2024-02-01 16:01:30 +0900

    ✨ Feat: 반려동물 정보 입력 페이지 구성

--- src/page/petInfo/PetInfo.tsx
+++ src/page/petInfo/PetInfo.tsx
@@ -1,58 +1,52 @@
 import React from 'react';
-import "./PetInfo.css"
+import './PetInfo.css';
 
 function PetInfo() {
   return (
-    <div className="container mx-auto p-4">
-      <div className="form-box bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="form-element mb-4">
-          <input className="form-input" id="name" type="text" placeholder="이름:" />
-        </div>
-        <div className="form-element mb-4">
-          <select className="form-select" id="gender">
-            <option value="" disabled selected>
-              성별:
-            </option>
-            <option value="male">수컷</option>
-            <option value="female">암컷</option>
-          </select>
-        </div>
-        <div className="form-element mb-4">
-          <input className="form-input" id="pet_type" type="text" placeholder="반려동물 종:" />
-        </div>
-        <div className="form-element mb-4">
-          <select className="form-select" id="neutered">
-            <option value="" disabled selected>
-              중성화 유무:
-            </option>
-            <option value="yes">O</option>
-            <option value="no">X</option>
-          </select>
-        </div>
-        <div className="form-element mb-4">
-          <input className="form-input" id="weight" type="text" placeholder="몸무게:" />
-        </div>
-        <div className="form-element mb-4">
-          <input className="form-input" id="birthday" type="date" placeholder="생일:" />
-        </div>
-        <div className="form-element mb-6">
-          <select className="form-select" id="blood_type">
-            <option value="" disabled selected>
-              혈액형:
-            </option>
-            <option value="A">A</option>
-            <option value="B">B</option>
-            <option value="AB">AB</option>
-          </select>
-        </div>
-        <div className="form-actions flex items-center justify-between">
-          <button
-            className="submit-btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
-          >
-            제출
-          </button>
-        </div>
+    <div className="container">
+      <div className='petInfoTitle'>반려동물 정보</div>
+      <div className="petName">
+        <input className="petNameInput" id="name" type="text" placeholder="이름:" />
+      </div>
+      <div className="petGender">
+        <select className="petGenderSelect" id="gender">
+          <option value="" disabled selected>
+            성별:
+          </option>
+          <option value="male">수컷</option>
+          <option value="female">암컷</option>
+        </select>
+      </div>
+      <div className="petType">
+        <input className="petTypeInput" id="pet_type" type="text" placeholder="반려동물 종:" />
+      </div>
+      <div className="petNeutered">
+        <select className="petNeuteredSelect" id="neutered">
+          <option value="" disabled selected>
+            중성화 유무:
+          </option>
+          <option value="yes">O</option>
+          <option value="no">X</option>
+        </select>
+      </div>
+      <div className="petWeight">
+        <input className="petWeightInput" id="weight" type="text" placeholder="몸무게:" />
+      </div>
+      <div className="petBirth">
+        <input className="petBirthInput" id="birthday" type="date" placeholder="생일:" />
+      </div>
+      <div className="petBlood">
+        <select className="petBloodSelect" id="blood_type">
+          <option value="" disabled selected>
+            혈액형:
+          </option>
+          <option value="A">A</option>
+          <option value="B">B</option>
+          <option value="AB">AB</option>
+        </select>
+      </div>
+      <div className="petInfoButton">
+        <button className="petInfoBtn">제출</button>
       </div>
     </div>
   );
