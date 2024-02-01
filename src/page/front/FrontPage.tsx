ecd3ddbee351540856b8f510f3d9c019bbc3a46e
Yusmin97 <tmdamsi97@gmail.com>
2024-02-01 09:37:08 +0900

    ✨ Feat: 진입 페이지 컴포넌트 제작
    FrontPage.tsx

---
+++ src/page/front/FrontPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./FrontPage.css"
+
+function FrontPage() {
+  return (
+    <div className="container">
+      <div className="cat_logo"></div>
+      <div className="login">
+        <button className="login_btn">로그인</button>
+      </div>
+      <div className="join">
+        <button className="join_btn">계정 없을 시 | 회원가입</button>
+      </div>
+    </div>
+  );
+}
+
+export default FrontPage;
