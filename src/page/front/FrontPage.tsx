38fb00c392d74fe0d1af46dd87e3538ad73b34ce
Yusmin97 <tmdamsi97@gmail.com>
2024-02-01 14:27:09 +0900

    ✨ Feat: 로그인 & 회원가입 페이지 이동하는 useNavigate 설정

--- src/page/front/FrontPage.tsx
+++ src/page/front/FrontPage.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 import "./FrontPage.css"
 
 function FrontPage() {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate('/login');
+  };
+
+  const handleSignUpClick = () => {
+    navigate('/signup');
+  };
+
   return (
     <div className="container">
       <div className="cat_logo"></div>
       <div className="front_login">
-        <button className="front_login_btn">로그인</button>
+        <button className="front_login_btn" onClick={handleLoginClick}>로그인</button>
       </div>
       <div className="join">
-        <button className="join_btn">계정 없을 시 | 회원가입</button>
+        <p className='joins_p'>계정 없을 시 |</p>
+        <button className="join_btn" onClick={handleSignUpClick}>회원가입</button>
       </div>
     </div>
   );
