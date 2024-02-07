a31a6e7385d776474f07130e5354f6b98cb21f0e
Yusmin97 <tmdamsi97@gmail.com>
2024-02-07 17:25:42 +0900

    ✨ Feat: 라우트 분리

--- server.ts
+++ server.ts
@@ -1,8 +1,7 @@
 import express, { Express } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { Request, Response } from 'express';
-import { petQuery } from './src/database/myPet';
+import signupRT from './route/signupRoute/signupRT'
 
 const app: Express = express();
 const port: number = 3001;
@@ -12,21 +11,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // 회원가입 API 엔드포인트
-app.post('/signup', async (req: Request, res: Response) => {
-  const { user_id, user_pw, user_name } = req.body;
-
-  try {
-    // 데이터베이스에 회원 정보 삽입
-    const sql = `INSERT INTO users (user_id, user_pw, user_name) VALUES (?, ?, ?)`;
-    await petQuery(sql, [user_id, user_pw, user_name]);
-
-    console.log('User signed up successfully');
-    res.status(200).json({ success: true, message: '회원가입에 성공했습니다.' });
-  } catch (error) {
-    console.error('Error signing up:', error);
-    res.status(500).json({ success: false, message: '회원가입에 실패했습니다.' });
-  }
-});
+app.use('/', signupRT)
 
 // 서버 시작
 app.listen(port, () => {
