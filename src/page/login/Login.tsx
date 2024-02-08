c274c7be7ff3920dca541226dcefadd64b3387ec
Yusmin97 <tmdamsi97@gmail.com>
2024-02-08 12:58:33 +0900

    ✨ Feat: 로그인 성공시 alert로 성공 여부 확인

--- src/page/login/Login.tsx
+++ src/page/login/Login.tsx
@@ -10,7 +10,7 @@ interface LoginData {
 const LoginPage: React.FC = () => {
   const [userData, setUserData] = useState<LoginData>({
     user_id: '',
-    user_pw: ''
+    user_pw: '',
   });
 
   // 사용자가 값을 입력할 때마다 해당 입력 필드의 상태를 업데이트
@@ -21,10 +21,12 @@ const LoginPage: React.FC = () => {
 
   const handleLogin = () => {
     // 입력된 ID와 비밀번호를 서버로 전송하여 로그인 시도
-    axios.post('http://localhost:3001/login', userData)
+    axios
+      .post('http://localhost:3001/login', userData)
       .then((response) => {
         // 로그인 성공 시 필요한 작업 수행
         console.log(response.data);
+        alert('로그인에 성공했습니다.');
       })
       .catch((error) => {
         // 로그인 실패 시 에러 처리
@@ -36,27 +38,29 @@ const LoginPage: React.FC = () => {
   return (
     <div className="container">
       <div className="input_row1">
-        <input 
-          type="text" 
-          className="id" 
-          placeholder="ID" 
-          name="user_id" 
-          value={userData.user_id} 
-          onChange={handleInputChange} 
+        <input
+          type="text"
+          className="id"
+          placeholder="ID"
+          name="user_id"
+          value={userData.user_id}
+          onChange={handleInputChange}
         />
       </div>
       <div className="input_row2">
-        <input 
-          type="password" 
-          className="password" 
-          placeholder="Password" 
-          name="user_pw" 
-          value={userData.user_pw} 
-          onChange={handleInputChange} 
+        <input
+          type="password"
+          className="password"
+          placeholder="Password"
+          name="user_pw"
+          value={userData.user_pw}
+          onChange={handleInputChange}
         />
       </div>
       <div className="login">
-        <button className="login_btn" onClick={handleLogin}>로그인</button>
+        <button className="login_btn" onClick={handleLogin}>
+          로그인
+        </button>
       </div>
     </div>
   );
