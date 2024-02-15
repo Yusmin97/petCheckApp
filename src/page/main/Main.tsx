4ecb8ab7b75ca16aeefb6e693110bf13db5990a9
Yusmin97 <tmdamsi97@gmail.com>
2024-02-15 11:07:18 +0900

    ✏️ Rename: import {} 중괄호 띄어쓰기

--- src/page/main/Main.tsx
+++ src/page/main/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { useAuth } from '../../authContext/authProvider';
 import { useNavigate } from 'react-router';
 import './Main.css';
