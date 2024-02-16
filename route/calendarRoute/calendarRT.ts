49caea951da0dbec4c5174beab1950ba164f3b83
Yusmin97 <tmdamsi97@gmail.com>
2024-02-16 10:13:07 +0900

    ✨ Feat: 캘린더 라우트 설정과 서버 설정

--- route/calendarRoute/calendarRT.ts
+++ route/calendarRoute/calendarRT.ts
@@ -1,38 +1,34 @@
 import { Request, Response, Router } from 'express';
-import mypetQuery from '../../src/database/myPet';
+import mypetQuery from '../../src/database/myPet'; // 외부에서 가져와야 합니다.
 
 const router: Router = Router();
 
-// 캘리더 이벤트 추가 API 엔드포인트
-router.post('/add-event', async (req: Request, res: Response) => {
-  const { title, description, start_date, end_date, location } = req.body;
+// 캘린더 일정 추가 API 엔드포인트
+router.post('/add-schedule', async (req: Request, res: Response) => {
+  const { title, description, start_date, end_date } = req.body;
 
   try {
-    // 쿼리 실행
-    await mypetQuery('INSERT INTO events (title, description, start_date, end_date, location) VALUES (?, ?, ?, ?, ?)', [
+    await mypetQuery('INSERT INTO calendar_schedule (title, description, start_date, end_date) VALUES (?, ?, ?, ?)', [
       title,
       description,
       start_date,
       end_date,
-      location
     ]);
-
     res.status(201).json({ message: '일정 추가가 성공적으로 완료되었습니다.' });
-  } catch (err) {
-    console.error('일정 추가 중 오류 발생:', err);
-    res.status(500).json({ error: '서버 오류가 발생했습니다.' });
+  } catch (error) {
+    console.error('일정 추가 중 오류 발생:', error);
+    res.status(500).json({ error: '일정 추가 중 오류가 발생했습니다.' });
   }
 });
 
-// 캘린더 이벤트 가져오기 API 엔드포인트
-router.get('/get-events', async (req: Request, res: Response) => {
-  
+// 캘린더 일정 불러오기 API 엔드포인트
+router.get('/get-schedules', async (req: Request, res: Response) => {
   try {
-    const events = await mypetQuery('SELECT * FROM events');
-    res.status(200).json(events);
+    const schedules = await mypetQuery('SELECT * FROM calendar_schedule', []);
+    res.status(200).json(schedules);
   } catch (error) {
-    console.error('이벤트 가져오기 중 오류 발생:', error);
-    res.status(500).json({ error: '이벤트를 가져오는 중 오류가 발생했습니다.' });
+    console.error('일정 불러오기 중 오류 발생:', error);
+    res.status(500).json({ error: '일정 불러오기 중 오류가 발생했습니다.' });
   }
 });
 
--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import signuploginRT from './route/signup-loginRoute/signup-loginRT'
+import calendarRT from './route/calendarRoute/calendarRT'
 
 const app: Express = express();
 const port: number = 3001;
@@ -11,7 +12,9 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // 회원가입 API 엔드포인트
-app.use('/', signuploginRT)
+app.use('/', signuploginRT);
+// 캘린더 API 엔드포인트
+app.use('/', calendarRT);
 
 // 서버 시작
 app.listen(port, () => {
