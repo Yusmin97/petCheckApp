616e87fb66f1c12557932eb1c485635028fb9d8b
Yusmin97 <tmdamsi97@gmail.com>
2024-02-26 10:45:47 +0900

    🎨 Design:  petinfo.css 간소화

--- src/page/petInfo/PetInfo.tsx
+++ src/page/petInfo/PetInfo.tsx
@@ -61,7 +61,7 @@ function PetInfoForm() {
           <label htmlFor="emogi" className="placeholderEmogi">
             종류:
           </label>
-          <select className="petEmogiSelect" id="emogi" name="pet_emogi" onChange={handleChange}>
+          <select className="petEmogiSelect text-indent60" id="emogi" name="pet_emogi" onChange={handleChange}>
             <option value=""></option>
             <option value="1F436">강아지</option>
             <option value="1F431">고양이</option>
@@ -71,13 +71,13 @@ function PetInfoForm() {
           <label htmlFor="name" className="placeholderName">
             이름:
           </label>
-          <input className="petNameInput" id="name" type="text" name="pet_name" onChange={handleChange} />
+          <input className="petNameInput text-indent60" id="name" type="text" name="pet_name" onChange={handleChange} />
         </div>
         <div className="petGender">
           <label htmlFor="gender" className="placeholderGender">
             성별:
           </label>
-          <select className="petGenderSelect" id="gender" name="pet_gender" onChange={handleChange}>
+          <select className="petGenderSelect text-indent60" id="gender" name="pet_gender" onChange={handleChange}>
             <option value=""></option>
             <option value="male">수컷</option>
             <option value="female">암컷</option>
@@ -87,13 +87,13 @@ function PetInfoForm() {
           <label htmlFor="pet_type" className="placeholderType">
             반려동물 종:
           </label>
-          <input className="petTypeInput" id="pet_type" type="text" name="pet_type" onChange={handleChange} />
+          <input className="petTypeInput text-indent110" id="pet_type" type="text" name="pet_type" onChange={handleChange} />
         </div>
         <div className="petNeutered">
           <label htmlFor="neutered" className="placeholderNeutered">
             중성화 유무:
           </label>
-          <select className="petNeuteredSelect" id="neutered" name="pet_neutered" onChange={handleChange}>
+          <select className="petNeuteredSelect text-indent110" id="neutered" name="pet_neutered" onChange={handleChange}>
             <option value=""></option>
             <option value="yes">O</option>
             <option value="no">X</option>
@@ -103,19 +103,19 @@ function PetInfoForm() {
           <label htmlFor="weight" className="placeholderWeight">
             몸무게:
           </label>
-          <input className="petWeightInput" id="weight" type="text" name="pet_weight" onChange={handleChange} />
+          <input className="petWeightInput text-indent75" id="weight" type="text" name="pet_weight" onChange={handleChange} />
         </div>
         <div className="petBirth">
           <label htmlFor="birthday" className="placeholderBirth">
             생일:
           </label>
-          <input className="petBirthInput" id="birthday" type="date" name="pet_birth" onChange={handleChange} />
+          <input className="petBirthInput text-indent30" id="birthday" type="date" name="pet_birth" onChange={handleChange} />
         </div>
         <div className="petBlood">
           <label htmlFor="blood_type" className="placeholderBlood">
             혈액형:
           </label>
-          <select className="petBloodSelect" id="blood_type" name="pet_blood" onChange={handleChange}>
+          <select className="petBloodSelect text-indent75" id="blood_type" name="pet_blood" onChange={handleChange}>
             <option value=""></option>
             <option value="A">A</option>
             <option value="B">B</option>
