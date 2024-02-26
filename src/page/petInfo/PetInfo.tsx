c932d6b742d76fdeac02073cd29d8489c48e630d
Yusmin97 <tmdamsi97@gmail.com>
2024-02-26 16:41:13 +0900

    🎨 Design: padding 조정

--- src/page/petInfo/PetInfo.tsx
+++ src/page/petInfo/PetInfo.tsx
@@ -56,65 +56,65 @@ function PetInfoForm() {
     <div className="bg-yellow-100 w-screen h-screen flex justify-center items-center flex-col">
       <div className="w-64 h-16 flex justify-center items-center font-inter text-center text-xl">반려동물 정보</div>
       <form onSubmit={handleSubmit}>
-        <div className="bg-yellow-300 w-72 h-10 flex items-center relative rounded-full mt-2">
+        <div className="bg-yellow-300 w-80 h-10 flex items-center relative rounded-full mt-2">
           <label htmlFor="emogi" className="placeholderEmogi absolute top-1/2 left-4 transform -translate-y-1/2 pointer-events-none text-black">
             종류:
           </label>
-          <select className="petEmogiSelect w-65 h-10 font-inter text-base bg-transparent border-none pl-16" id="emogi" name="pet_emogi" onChange={handleChange}>
+          <select className="petEmogiSelect w-72 h-10 font-inter text-base bg-transparent border-none pl-14" id="emogi" name="pet_emogi" onChange={handleChange}>
             <option value=""></option>
             <option value="1F436">강아지</option>
             <option value="1F431">고양이</option>
           </select>
         </div>
-        <div className="bg-yellow-300 w-72 h-10 flex items-center relative rounded-full mt-2">
+        <div className="bg-yellow-300 w-80 h-10 flex items-center relative rounded-full mt-2">
           <label htmlFor="name" className="placeholderName absolute top-1/2 left-4 transform -translate-y-1/2 pointer-events-none text-black">
             이름:
           </label>
-          <input className="petNameInput w-65 h-10 font-inter text-base bg-transparent border-none pl-16" id="name" type="text" name="pet_name" onChange={handleChange} />
+          <input className="petNameInput w-72 h-10 font-inter text-base bg-transparent border-none pl-16" id="name" type="text" name="pet_name" onChange={handleChange} />
         </div>
-        <div className="bg-yellow-300 w-72 h-10 flex items-center relative rounded-full mt-2">
+        <div className="bg-yellow-300 w-80 h-10 flex items-center relative rounded-full mt-2">
           <label htmlFor="gender" className="placeholderGender absolute top-1/2 left-4 transform -translate-y-1/2 pointer-events-none text-black">
             성별:
           </label>
-          <select className="petGenderSelect w-65 h-10 font-inter text-base bg-transparent border-none pl-16" id="gender" name="pet_gender" onChange={handleChange}>
+          <select className="petGenderSelect w-72 h-10 font-inter text-base bg-transparent border-none pl-14" id="gender" name="pet_gender" onChange={handleChange}>
             <option value=""></option>
             <option value="male">수컷</option>
             <option value="female">암컷</option>
           </select>
         </div>
-        <div className="bg-yellow-300 w-72 h-10 flex items-center relative rounded-full mt-2">
+        <div className="bg-yellow-300 w-80 h-10 flex items-center relative rounded-full mt-2">
           <label htmlFor="pet_type" className="placeholderType absolute top-1/2 left-4 transform -translate-y-1/2 pointer-events-none text-black">
             반려동물 종:
           </label>
-          <input className="petTypeInput w-65 h-10 font-inter text-base bg-transparent border-none pl-28" id="pet_type" type="text" name="pet_type" onChange={handleChange} />
+          <input className="petTypeInput w-72 h-10 font-inter text-base bg-transparent border-none pl-28" id="pet_type" type="text" name="pet_type" onChange={handleChange} />
         </div>
-        <div className="bg-yellow-300 w-72 h-10 flex items-center relative rounded-full mt-2">
+        <div className="bg-yellow-300 w-80 h-10 flex items-center relative rounded-full mt-2">
           <label htmlFor="neutered" className="placeholderNeutered absolute top-1/2 left-4 transform -translate-y-1/2 pointer-events-none text-black">
             중성화 유무:
           </label>
-          <select className="petNeuteredSelect w-65 h-10 font-inter text-base bg-transparent border-none pl-32" id="neutered" name="pet_neutered" onChange={handleChange}>
+          <select className="petNeuteredSelect w-72 h-10 font-inter text-base bg-transparent border-none pl-28" id="neutered" name="pet_neutered" onChange={handleChange}>
             <option value=""></option>
             <option value="yes">O</option>
             <option value="no">X</option>
           </select>
         </div>
-        <div className="bg-yellow-300 w-72 h-10 flex items-center relative rounded-full mt-2">
+        <div className="bg-yellow-300 w-80 h-10 flex items-center relative rounded-full mt-2">
           <label htmlFor="weight" className="placeholderWeight absolute top-1/2 left-4 transform -translate-y-1/2 pointer-events-none text-black">
             몸무게:
           </label>
-          <input className="petWeightInput w-65 h-10 font-inter text-base bg-transparent border-none pl-20" id="weight" type="text" name="pet_weight" onChange={handleChange} />
+          <input className="petWeightInput w-72 h-10 font-inter text-base bg-transparent border-none pl-20" id="weight" type="text" name="pet_weight" onChange={handleChange} />
         </div>
-        <div className="bg-yellow-300 w-72 h-10 flex items-center relative rounded-full mt-2">
+        <div className="bg-yellow-300 w-80 h-10 flex items-center relative rounded-full mt-2">
           <label htmlFor="birthday" className="placeholderBirth absolute top-1/2 left-4 transform -translate-y-1/2 pointer-events-none text-black">
             생일:
           </label>
-          <input className="petBirthInput w-65 h-10 font-inter text-base bg-transparent border-none pl-32" id="birthday" type="date" name="pet_birth" onChange={handleChange} />
+          <input className="petBirthInput w-72 h-10 font-inter text-base bg-transparent border-none pl-20" id="birthday" type="date" name="pet_birth" onChange={handleChange} />
         </div>
-        <div className="bg-yellow-300 w-72 h-10 flex items-center relative rounded-full mt-2">
+        <div className="bg-yellow-300 w-80 h-10 flex items-center relative rounded-full mt-2">
           <label htmlFor="blood_type" className="placeholderBlood absolute top-1/2 left-4 transform -translate-y-1/2 pointer-events-none text-black">
             혈액형:
           </label>
-          <select className="petBloodSelect w-65 h-10 font-inter text-base bg-transparent border-none pl-20" id="blood_type" name="pet_blood" onChange={handleChange}>
+          <select className="petBloodSelect w-72 h-10 font-inter text-base bg-transparent border-none pl-20" id="blood_type" name="pet_blood" onChange={handleChange}>
             <option value=""></option>
             <option value="A">A</option>
             <option value="B">B</option>
